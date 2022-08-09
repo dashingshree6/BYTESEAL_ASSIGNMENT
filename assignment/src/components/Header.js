68812e15fd34281ee8ee3aedd389f30b86f48717
dashingshree6 <dashingshree6@gmail.com>
2022-08-09 19:53:24 +0530

    Create Header.js

---
+++ assignment/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="flex flex-row bg-gray-700 dark:bg-gray-200 border-b-2 dark:border-gray-700 pb-4 pt-4 font-bold">
+            <h1 className="w-screen text-3xl text-center text-slate-100 dark:text-gray-600">Dashboard </h1>  
+        </div>
+    )
+}
+export default Header;
